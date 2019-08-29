8211277d8d90301eb78eef88b96e349c1c34bde8
Yordi Ogando <yordiogando@gmail.com>
2019-08-29 00:46:47 -0400

    Foreign Keys and New Models

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tutorial
+from .models import Tutorial, TutorialCategory, TutorialSeries
 from tinymce.widgets import TinyMCE
 from django.db import models
 
@@ -13,6 +13,8 @@ class TutorialAdmin(admin.ModelAdmin):
    
     fieldsets = [
         ("Title/date", {"fields": ["tutorial_title", "tutorial_published"]}),
+        ("URL", {"fields": ["tutorial_slug"]}),
+        ("Series", {"fields": ["tutorial_series"]}),
         ("Content", {"fields": ["tutorial_content"]})
     ]
 
@@ -20,5 +22,7 @@ class TutorialAdmin(admin.ModelAdmin):
         models.TextField: {'widget': TinyMCE()}
     }
 
+admin.site.register(TutorialSeries)
+admin.site.register(TutorialCategory)
 
 admin.site.register(Tutorial, TutorialAdmin)
